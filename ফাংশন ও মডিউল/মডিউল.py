8b528ac114e8eac9ac1329e8858236fc07716141
joy <ziaurjoy802@gmail.com>
2021-01-27 02:31:00 +0600

    Functional Programming Done...

---
+++ ফাংশন ও মডিউল/মডিউল.py
@@ -0,0 +1,13 @@
+
+
+import random # import module
+random_number = random.randint(1, 10)
+print(random_number)
+
+# from math import sqrt # module to function import
+# print(sqrt(25))
+
+from math import sqrt as squre_root # module function name changeing
+
+# a = 25
+# print(squre_root(a))
